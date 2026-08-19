31e6692cfec98067554e637703b46d6ecd0d6a4f
PranshuUrmaliya2004 <pransuurmaliya2018@gmail.com>
2026-08-19 23:53:12 +0530

    second commit

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import HeroBanner from '../Components/HeroBanner'
-import LatestCollection from '../Components/latestCollection'
+import LatestCollection from '../Components/LatestCollection'
 import BestSeller from '../Components/BestSeller'
 import OurPolicy from '../Components/OurPolicy'
 import NewsletterBox from '../Components/NewsletterBox'
